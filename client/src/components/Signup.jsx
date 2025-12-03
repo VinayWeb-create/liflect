fb049d38367babbdec996a53e2b7e27f388b59b8
Avala vinay <avalavinay4@gmail.com>
2025-12-03 16:03:37 +0530

    Update signup API endpoint to production URL

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -20,7 +20,7 @@ const Signup = ({ onAuthSuccess }) => {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/signup", {
+      const res = await fetch("https://liflect-1.onrender.com/api/auth/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
